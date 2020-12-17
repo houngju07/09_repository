1dda86d506388aad1e2991473aae2541ec7e22a8
- <->
2020-12-17 18:29:07 +0900

    09_02
    09_2

--- main.c
+++ main.c
@@ -7,19 +7,17 @@
 int main(int argc, char *argv[]) {
 	
 	//array declareation
-	int i;
-	int grade[5];
+	int i, aerage;
+	int sum;
+	int grade[SIZE];
 	
-	//value input
-	grade[0]=10;
-	grade[1]=20;
-	grade[2]=30;
-	grade[3]=40;
-	grade[4]=50;
-					
-	//print
-	for(i=0;i<5;i++)
-	printf("grade[%d]=%d\n", i, grade[i]);
+	printf("input %i score\n", SIZE);
+	
+	for(i=0;i<SIZE;i++)
+		scanf("%d", &grade[i]);
+		
+	for(i=0;i<SIZE;i++)
+		printf("grade[%d]=%d\n", i, grade[i]);
 	
 	
 	return 0;
